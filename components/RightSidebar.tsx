c3fa5bae5a8747ef366dca7a5b1fe4059400dfa4
alfinantony <alfinantony77@gmail.com>
2024-05-27 23:03:01 +0530

    Initial commit

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps)=> {
+  return (
+    <div>
+      rIGHt side bar
+    </div>
+  )
+}
+
+export default RightSidebar
